1a2eadb629b99dfe48e56eecacf273ad357b276f
Vetaliy <pastuh.vitalik98@gmail.com>
2018-10-29 16:21:21 +0200

    Models was improved

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -2,5 +2,4 @@ class Faculty < ApplicationRecord
   has_many :specialty
   has_many :teachers
   belongs_to :university
-  has_many :admins
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ApplicationRecord
+  extend Enumerize
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +8,13 @@ class Student < ApplicationRecord
   has_many :student_subjects
   has_many :subjects, through: :student_subjects
   belongs_to :group
+
+  ROLES =
+      { student: '0', admin: '1' }.with_indifferent_access.freeze
+  enumerize :roles,
+            in: ROLES.keys, scope: true, default: :student
+  ROLES.each_key do |role_|
+    define_method("#{role_}?") { role.include?(role_) }
+  end
+
 end
