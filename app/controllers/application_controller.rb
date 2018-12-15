826b46938d95a4cad52c409aae7f601bbe19c5d8
Vetaliy <pastuh.vitalik98@gmail.com>
2018-12-15 11:32:46 +0200

    Add ability for each users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_ability
+    if current_student
+      @current_ability ||= Ability.new(current_student)
+    elsif current_employer
+      @current_ability ||= Ability.new(current_employer)
+    end
+  end
 end
