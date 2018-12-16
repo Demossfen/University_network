4d5f0e9880c0a19d4b6b554453fa14dc8e8ef48f
Vetaliy <pastuh.vitalik98@gmail.com>
2018-12-16 20:00:13 +0200

    Add status to employer

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,7 +1,17 @@
 class Employer < ApplicationRecord
+  include FullName
+  extend Enumerize
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :company
 
-  validates :first_name, :last_name, :email, presence: true
+  STATUSES = { new: 0, approved: 1, banned: 2 }.with_indifferent_access.freeze
+
+  enumerize :status, in: STATUSES.keys, scope: true, default: :new
+
+  STATUSES.each_key do |_status|
+    define_method("#{_status}?") { status == _status }
+  end
+
+  validates :first_name, :last_name, :email, :status, presence: true
 end
